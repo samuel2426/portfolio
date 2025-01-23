2fa5c0bceb3fbcff4b9f736d2f02bdcd6e514386
samuel2426 <samuel.pg2426@gmail.com>
2025-01-23 23:52:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import *
+import os
 
 app=Flask(__name__)
 
@@ -6,4 +7,6 @@ app=Flask(__name__)
 def home():
     return render_template('index.html')
 
-app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Get the port from environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port,debug=True)
